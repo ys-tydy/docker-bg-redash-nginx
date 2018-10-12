2486e8f7b4471495e6caaaa477649bf1813c6738
Yuji Sakurai <yuji_sakurai@r.recruit.co.jp>
2018-10-12 19:23:33 +0900

    fix

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ REDASH_HOST=${REDASH_HOST:-localhost}
 REDASH_SERVER_PORT=${REDASH_SERVER_PORT:-5000}
 LISTEN_PORT=${LISTEN_PORT:-80}
 
-sed -i -e "s/***LISTEN_PORT***/${LISTEN_PORT}/g" ./nginx_template.conf
-sed -i -e "s/***REDASH_HOST***/${REDASH_HOST}/g" ./nginx_template.conf
-sed -e "s/***REDASH_SERVER_PORT***/${REDASH_SERVER_PORT}/g" ./nginx_template.conf > /etc/nginx/nginx.conf
+sed -i -e "s/---LISTEN_PORT---/${LISTEN_PORT}/g" ./nginx_template.conf
+sed -i -e "s/---REDASH_HOST---/${REDASH_HOST}/g" ./nginx_template.conf
+sed -e "s/---REDASH_SERVER_PORT---/${REDASH_SERVER_PORT}/g" ./nginx_template.conf > /etc/nginx/nginx.conf
 nginx
